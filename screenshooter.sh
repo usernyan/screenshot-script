2acd41c1b00ae5944c8819fc1d7b91daf28eacc8
usernyan <nobody@example.com>
2022-08-28 05:09:31 -0400

    Added script outline

---
+++ screenshooter.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#dependencies:
+#	shotgun
+#	hacksaw
+#	xclip
+#	date
+
+#help:
+while getopts "h" OPTION; do
+	case "$OPTION" in
+		h) #display help
+			echo "Not implemented yet."
+			exit;;
+	esac
+done
+
+
+#do stuff
